d77f8f7fb90f136f2e917a4610083d7a3aeb3ca2
FuturoDevJunior <196907994+FuturoDevJunior@users.noreply.github.com>
2025-02-24 06:01:03 -0300

    Projeto fullstackchallenge completo e testado

--- frontend/src/app.js
+++ frontend/src/app.js
@@ -5,26 +5,16 @@ import Orders from './pages/Orders';
 import Dashboard from './pages/Dashboard';
 
 function App() {
-    return ( <
-        Router >
-        <
-        Routes >
-        <
-        Route path = "/products"
-        element = { < Products / > }
-        /> <
-        Route path = "/categories"
-        element = { < Categories / > }
-        /> <
-        Route path = "/orders"
-        element = { < Orders / > }
-        /> <
-        Route path = "/dashboard"
-        element = { < Dashboard / > }
-        /> <
-        /Routes> <
-        /Router>
-    );
+  return (
+    <Router>
+      <Routes>
+        <Route path="/products" element={<Products />} />
+        <Route path="/categories" element={<Categories />} />
+        <Route path="/orders" element={<Orders />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
